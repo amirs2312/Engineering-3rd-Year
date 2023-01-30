36f4a125e02a7a17af744146646a84835ba0fa6a
Saad Amir <amirs@tcd.ie>
2023-01-30 14:04:01 +0000

    Update lab01.c

--- labs/lab01/lab01.c
+++ labs/lab01/lab01.c
@@ -1,39 +1,38 @@
+
 #include "pico/stdlib.h"
 
-//Saad Amir 20334372
+///function to repeatedly cause the raspberry pi to blink
+void repeat_blink(int sleep_delay, int pinnumber);
 
-/**
- * @brief EXAMPLE - BLINK_C
- *        Simple example to initialise the built-in LED on
- *        the Raspberry Pi Pico and then flash it forever. 
- * 
- * @return int  Application return code (zero for success).
- */
 int main() {
 
-    // Specify the PIN number and sleep delay
+/// Declare the Pin number and sleep delay
     const uint LED_PIN   =  25;
     const uint LED_DELAY = 500;
 
-    // Setup the LED pin as an output.
+
+///Setup the LED pin as an output
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    // Do forever...
-    while (true) {
 
-        // Toggle the LED on and then sleep for delay period
-        gpio_put(LED_PIN, 1);
-        sleep_ms(LED_DELAY);
+/// Call the repeat blink function 
+    repeat_blink(LED_DELAY,LED_PIN);
 
-        // Toggle the LED off and then sleep for delay period
-        gpio_put(LED_PIN, 0);
-        sleep_ms(LED_DELAY);
-
-    }
-
-    // Should never get here due to infinite while-loop.
+///return 0 when main is finished, if sucessful this point should never be reached.
     return 0;
-
 }
 
+void repeat_blink(int sleep_delay, int pinnumber){
+
+       while (true) {
+        gpio_put(pinnumber, 1);
+        sleep_ms(sleep_delay);
+    ///Toggle the LED on and then sleep for delay period
+        
+        gpio_put(pinnumber, 0);
+        sleep_ms(sleep_delay);
+    /// Toggle the LED off and then sleep for delay period
+    }
+
+}
