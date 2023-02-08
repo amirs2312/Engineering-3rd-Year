ccf4ebc74fb4caf0a91b9c2eb098fc3c25b52c83
Saad Amir <amirs@tcd.ie>
2023-02-08 07:37:20 +0000

    Update lab02.c

--- labs/lab02/lab02.c
+++ labs/lab02/lab02.c
@@ -6,14 +6,57 @@
 #include "pico/double.h"    // Required for using double-precision variables.
 
 
-/**
- * @brief LAB #02 - TEMPLATE
- *        Main entry point for the code.
- * 
- * @return int      Returns exit-status zero on completion.
- */
-int main() {
+double Wallis(int MAX_IT);
+float Wallis_f(int MAX_IT);
 
-    // Returning zero indicates everything went okay.
+int main(){
+    double approximation;
+    double REFERENCE_PI = 3.14159265359;
+    float approximation_f;
+
+    approximation = Wallis(10000);
+    approximation_f = Wallis_f(10000);
+
+    double error = 100*(2*approximation-REFERENCE_PI)/REFERENCE_PI;
+    float errorf = 100*(2*approximation_f-REFERENCE_PI)/REFERENCE_PI;
+
+    printf("Half of pi is equal to %lf\n",approximation);
+    printf("Error of %lf percent\n\n",error);
+    printf("The float approximation of half of pi is equal to %f\n",approximation_f);
+     printf("Error of %f percent\n",errorf);
+
+
     return 0;
+}
+
+
+
+
+
+double Wallis(int MAX_IT){
+    double half_calculated_pi = 1;
+    
+
+    for(int i = 1;i<=MAX_IT;i++){
+        half_calculated_pi = half_calculated_pi*(4*i*i)/((4*i*i)-1);
+    }
+
+    
+
+    return half_calculated_pi;
+    
+}
+
+float Wallis_f(int MAX_IT){
+    float half_calculated_pi = 1;
+    
+
+    for(int i = 1;i<=MAX_IT;i++){
+        half_calculated_pi = half_calculated_pi*(4*i*i)/((4*i*i)-1);
+    }
+
+  
+
+    return half_calculated_pi;
+    
 }
