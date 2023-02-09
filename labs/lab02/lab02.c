6d6ba06966a7fdbb639d311fc2caccfc0f29d3db
Saad Amir <amirs@tcd.ie>
2023-02-09 01:28:49 +0000

    Update lab02.c

--- labs/lab02/lab02.c
+++ labs/lab02/lab02.c
@@ -1,31 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "pico/stdlib.h"
-#include "pico/float.h"     // Required for using single-precision variables.
-#include "pico/double.h"    // Required for using double-precision variables.
+//#include "pico/stdlib.h"
 
+/// Required for using single-precision variables.
+/// Required for using double-precision variables.
+#include "pico/float.h"    
+#include "pico/double.h"   
 
+/// Calculates the the value of the Wallis product.
+/// The Wallis product is an infinte product approximation of pi/2.
+/// This function approximates it using the double variable, continuing the product 'MAX_IT' times.
 double Wallis(int MAX_IT);
+
+
+/// Calculates the the value of the Wallis product.
+/// The Wallis product is an infinte product approximation of pi/2.
+/// This function approximates it using the float variable, continuing the product 'MAX_IT' times.
 float Wallis_f(int MAX_IT);
 
 int main(){
-    double approximation;
-    double REFERENCE_PI = 3.14159265359;
-    float approximation_f;
+    
+    /// The double approximatin for pi/2.
+    double approximation_d;
 
-    approximation = Wallis(10000);
+    /// pi accurate to 11 decimal places 
+    double REFERENCE_PI = 3.14159265359; 
+
+    /// The float approximatin for pi/2.
+    float approximation_f; 
+
+    /// Calculate the value of the double approximation of pi/2 using 100000 iterations.
+    approximation_d = Wallis(10000);
+
+    /// Calculate the value of the float approximation of pi/2 using 100000 iterations.
     approximation_f = Wallis_f(10000);
 
-    double error = 100*(2*approximation-REFERENCE_PI)/REFERENCE_PI;
+
+    /// Calculate and assign the percentage error between the double approximation
+    /// of pi and the reference value of pi.
+    double error = 100*(2*approximation_d-REFERENCE_PI)/REFERENCE_PI;
+
+    /// Calculate and assign the percentage error between the float approximation
+    /// of pi and the reference value of pi.
     float errorf = 100*(2*approximation_f-REFERENCE_PI)/REFERENCE_PI;
 
-    printf("Half of pi is equal to %lf\n",approximation);
+    ///Print out the relevent metrics.
+    printf("The double approximation of half of pi is equal to %lf\n",approximation_d);
     printf("Error of %lf percent\n\n",error);
     printf("The float approximation of half of pi is equal to %f\n",approximation_f);
-     printf("Error of %f percent\n",errorf);
+    printf("Error of %f percent\n",errorf);
 
-
+    /// Return 0 to indicate the program ran successfully.
     return 0;
 }
 
@@ -34,29 +60,35 @@ int main(){
 
 
 double Wallis(int MAX_IT){
+
+    /// The value of the infinte product as a double, initialised at 1.
     double half_calculated_pi = 1;
     
-
+    /// Calculate the value of the infinte product.
     for(int i = 1;i<=MAX_IT;i++){
         half_calculated_pi = half_calculated_pi*(4*i*i)/((4*i*i)-1);
     }
 
     
-
+    /// Return the value of the infinte product.
     return half_calculated_pi;
     
 }
 
 float Wallis_f(int MAX_IT){
+
+    /// The value of the infinte product as a double, initialised at 1.
     float half_calculated_pi = 1;
     
-
+    /// Calculate the value of the infinte product.
     for(int i = 1;i<=MAX_IT;i++){
         half_calculated_pi = half_calculated_pi*(4*i*i)/((4*i*i)-1);
     }
 
   
-
+     /// Return the value of the infinte product.
     return half_calculated_pi;
     
 }
+
+
